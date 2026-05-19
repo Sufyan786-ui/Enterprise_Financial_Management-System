64841dccf85847ed1d4f9133d2faf30c5a3c7191
Sufyan <ahmedsufiyansaleem@gmail.com>
2026-05-19 21:07:05 +0530

    First Commit

---
+++ Enterprise_Managment_System/src/main/java/com/MiniProject/Enterprise_Managment_System/EnterpriseManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.MiniProject.Enterprise_Managment_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ Enterprise_Managment_System/src/test/java/com/MiniProject/Enterprise_Managment_System/EnterpriseManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MiniProject.Enterprise_Managment_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
